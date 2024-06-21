f4e07d8fbc8feef9f9ccb8b91b026dfd944f53ea
Abdirizackmata <abdirizackissack2018@gmail.com>
2024-06-21 09:58:13 -0700

    modified the footer layout design

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,7 +1,5 @@
 import React from "react";
-import { footerVariants, staggerChildren } from "../../utils/motion";
-import css from "./Footer.module.scss";
-import { motion } from "framer-motion";
+import css from "./Footer.module.css";
 import WhatsAppIcon from "@mui/icons-material/WhatsApp";
 import FacebookIcon from "@mui/icons-material/Facebook";
 import InstagramIcon from "@mui/icons-material/Instagram";
@@ -9,19 +7,10 @@ import GitHubIcon from "@mui/icons-material/GitHub";
 import TwitterIcon from "@mui/icons-material/Twitter";
 const Footer = () => {
   return (
-    <motion.section
-      variants={staggerChildren}
-      initial="hidden"
-      whileInView="show"
-      viewport={{ once: false, amount: 0.25 }}
-      className={css.wrapper}
-    >
-      <motion.div
-        variants={footerVariants}
-        className={`innerWidth yPaddings flexCenter ${css.container}`}
-      >
-        <div className={css.left}>
-          <span>
+    <section className={css.footer}>
+      <div>
+        <div className={css.upper}>
+          <span >
             Let's make something <br />
             amazing together.
           </span>
@@ -30,46 +19,12 @@ const Footer = () => {
             <a href="mailto:abdirizackissack2018@gmail.com">saying hi</a>
           </span>
         </div>
-        {/* center/Social links */}
-        <div className={`paddings flexCenter ${css.social}`}>
-          <h1>My Social Media</h1>
-          <div className={css.socialLinks}>
-            <ul className="flexCenter">
-              <li>
-                <a href="https://wa.me/+254719455494">
-                  <WhatsAppIcon className={css.icon}/>
-                </a>
-              </li>
-              <li>
-                <a href="https://www.facebook.com/abdirizack.issacksiyat">
-                  <FacebookIcon className={css.icon}/>
-                </a>
-              </li>
-              <li>
-                <a href="https://www.instagram.com/abdulrazaq_masta/">
-                  <InstagramIcon className={css.icon}/>
-                </a>
-              </li>
-              <li>
-                <a href="https://twitter.com/ibnu_isxaq">
-                  <TwitterIcon className={css.icon}/>
-                </a>
-              </li>
-              <li>
-                <a href="https://github.com/Abdirizackmasta">
-                  <GitHubIcon className={css.icon}/>
-                </a>
-              </li>
-            </ul>
-          </div>
-        </div>
-
-        <div className={css.right}>
-          <div className={css.info}>
-            <span className="secondaryText">Information</span>
+        <div className={css.middle}>
+          <div>
+            <span className={css.header}>Information</span>
             <p>Nairobi, Easteligh, 12th Street, Kenya</p>
           </div>
-          <ul className={css.menu}>
+          <ul>
             <li>
               <a href="#experties">Services</a>
             </li>
@@ -84,8 +39,40 @@ const Footer = () => {
             </li>
           </ul>
         </div>
-      </motion.div>
-    </motion.section>
+        {/* center/Social links */}
+        <hr />
+        <div className={css.social}>
+          <ul>
+              <li>
+                <a href="https://wa.me/+254719455494">
+                  <WhatsAppIcon />
+                </a>
+              </li>
+              <li>
+                <a href="https://www.facebook.com/abdirizack.issacksiyat">
+                  <FacebookIcon />
+                </a>
+              </li>
+              <li>
+                <a href="https://www.instagram.com/abdulrazaq_masta/">
+                  <InstagramIcon />
+                </a>
+              </li>
+              <li>
+                <a href="https://twitter.com/ibnu_isxaq">
+                  <TwitterIcon />
+                </a>
+              </li>
+              <li>
+                <a href="https://github.com/Abdirizackmasta">
+                  <GitHubIcon />
+                </a>
+              </li>
+            </ul>
+        </div>
+      </div>
+       
+    </section>
   );
 };
 
